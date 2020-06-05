30e56a2e8e9679c3265cc6e74fd51b01a4f423fc
Zoltan Nebli <zoltan.nebli@dpc.hu>
2020-06-05 14:50:15 +0200

    moved importer app api part to identity app

--- src/main/java/org/apache/fineract/organisation/parent/AbstractPersistableCustom.java
+++ src/main/java/org/apache/fineract/organisation/parent/AbstractPersistableCustom.java
@@ -6,9 +6,9 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License. You may obtain a copy of the License at
- *
+ * <p>
  * http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
@@ -18,55 +18,36 @@
  */
 package org.apache.fineract.organisation.parent;
 
-import java.io.Serializable;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import org.springframework.data.domain.Persistable;
 
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
-
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import org.springframework.data.domain.Persistable;
+import java.io.Serializable;
 
 
 @MappedSuperclass
 public abstract class AbstractPersistableCustom<PK extends Serializable> implements Persistable<Long> {
 
-        @Id
-        @GeneratedValue(strategy = GenerationType.IDENTITY)
-        private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-        /*
-         * (non-Javadoc)
-         * 
-         * @see org.springframework.data.domain.Persistable#getId()
-         */
-        @Override
-        public Long getId() {
-                return id;
-        }
+    @Override
+    public Long getId() {
+        return id;
+    }
 
-        /**
-         * Sets the id of the entity.
-         * 
-         * @param id the id to set
-         */
-        public void setId(final Long id) {
+    public void setId(final Long id) {
 
-                this.id = id;
-        }
+        this.id = id;
+    }
 
-        /*
-         * (non-Javadoc)
-         *
-         * @see org.springframework.data.domain.Persistable#isNew()
-         */
-        @Override
-        @JsonIgnore
-        public boolean isNew() {
-
-                return null == this.id;
-        }
-
-        //We have removed toString(), hashCode() and equals() methods. By adding them end up issues with OpenJPA
+    @Override
+    @JsonIgnore
+    public boolean isNew() {
+        return null == this.id;
+    }
 }
